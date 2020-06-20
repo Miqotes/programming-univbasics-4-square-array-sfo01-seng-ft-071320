7c723713ac8c2d4647e0c9bf2ab21b708aa7c4b8
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-20 00:54:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,7 @@
 def square_array(array)
-  
-  array.each { |nums| nums ** 2}
-  
-  
+  counter = 0
+  while counter < array.length do 
+  puts array.sqrt[counter]
+  counter += 1
+  end
 end
-#counter = 0
-  #while counter < array.length do 
-  #puts array.sqrt[counter]
-  #counter += 1
-  #end
